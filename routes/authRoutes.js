94d7621f3259487040883cdfc4ca767937299b23
JuanR66803 <juan.reinosa@correounivalle.edu.co>
2025-03-24 09:42:53 -0500

    correcion de errores

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,5 +5,9 @@ const router = express.Router();
 
 router.post("/register", registerUser);
 
+// Ruta de prueba
+router.get("/test", (req, res) => {
+    res.json({ message: "El servidor está funcionando correctamente 🚀" });
+});
 
 export default router;
